147ee8c96cc6e36d2710c09ab0a12d664a9bdf4e
Setepenre <pierre.delaunay@outlook.com>
2023-02-03 18:33:30 -0500

    Remove 3.7 add 3.11

--- setup.py
+++ setup.py
@@ -31,10 +31,10 @@ setup_kwargs = dict(
     ],
     classifiers=[
         "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Operating System :: OS Independent",
         "Development Status :: 4 - Beta",
     ],
