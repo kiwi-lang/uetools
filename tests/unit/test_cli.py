55aae8c1ebca92a7d0eac9e33c0204fd94d45396
Setepenre <pierre.delaunay@outlook.com>
2023-07-09 11:19:15 -0400

    Update error checking

--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -1,17 +1,12 @@
-import pytest
-
 from uetools.core.cli import args, main
 
 
 def test_cli():
-    with pytest.raises(SystemExit) as err:
-        # Show all the commands
-        main(args("--help"))
+    # Show all the commands
+    assert main(args("--help")) == 0
 
-        # Show all the editor related commands
-        main(args("editor", "--help"))
+    # Show all the editor related commands
+    assert main(args("editor", "--help")) == 0
 
-        # Show the help for a command in particular
-        main(args("editor", "editor", "--help"))
-
-    assert err.value.code == 0
+    # Show the help for a command in particular
+    assert main(args("editor", "editor", "--help")) == 0
