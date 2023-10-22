3f56653d4e6a22bca786bea7125de43d8cd69f43
Setepenre <pierre.delaunay@outlook.com>
2023-10-22 14:01:40 -0400

    Update gitlab publish

--- uetools/commands/gitlab/publish.py
+++ uetools/commands/gitlab/publish.py
@@ -45,6 +45,37 @@ class ChunkUploader:
         return self.totalsize
 
 
+def upload_package(
+    src,
+    api_url,
+    project_id,
+    package_name,
+    package_version,
+    filename,
+    token,
+    chunk_size=1024,
+):
+    # PUT /projects/:id/packages/generic/:package_name/:package_version/:file_name?status=:status
+    url = f"{api_url}/projects/{project_id}/packages/generic/{package_name}/{package_version}/{filename}"
+
+    if os.getenv("CI_JOB_TOKEN") is not None:
+        headers = {"JOB-TOKEN": token}
+    else:
+        headers = {"Authorization": f"Bearer {token}"}
+
+    print("URL: ", url)
+    response = requests.put(
+        url,
+        headers=headers,
+        data=ChunkUploader(src, chunk_size),
+    )
+
+    if response.status_code == 200:
+        return 0
+
+    raise RuntimeError(response.text)
+
+
 class Publish(Command):
     """Publish a gitlab package to the registry"""
 
@@ -60,7 +91,7 @@ class Publish(Command):
 
         api_url: str      = os.getenv("CI_API_V4_URL", default_url)
         project_id: str   = os.getenv("CI_PROJECT_ID")
-        commit_tag: str   = os.getenv("CI_COMMIT_TAG")
+        commit_tag: str   = os.getenv("CI_COMMIT_TAG", "v0.0.0")
         commit_short: str = os.getenv("CI_COMMIT_SHORT_SHA")
         token: str        = os.getenv("CI_JOB_TOKEN")
         # fmt: on
@@ -72,33 +103,24 @@ class Publish(Command):
 
         project = project_name
         platform = args.platform
-
-        file_path: str = args.filename
-        chunk_size = args.chunk
-
-        ext = file_path.rsplit(".", maxsplit=1)[1]
+        ext = args.filename.rsplit(".", maxsplit=1)[1]
 
         package_name = f"{project}"
         package_version = f"{platform}-{args.commit_short}"
         filename = f"{project}-{args.commit_tag}.{ext}"
 
-        # PUT /projects/:id/packages/generic/:package_name/:package_version/:file_name?status=:status
-        url = f"{args.api_url}/projects/{args.project_id}/packages/generic/{package_name}/{package_version}/{filename}"
-
-        headers = {"JOB-TOKEN": args.token}
-
-        print("URL: ", url)
-        response = requests.put(
-            url, 
-            headers=headers, 
-            data=ChunkUploader(file_path, chunk_size),
+        upload_package(
+            args.filename, 
+            args.api_url, 
+            args.project_id, 
+            package_name, 
+            package_version, 
+            filename, 
+            args.token, 
+             args.chunk,
         )
 
-        if response.status_code == 200:
-            return 0
-
-        print(response.text)
-        return -1
+        return 0
 
 
 COMMANDS = Publish
