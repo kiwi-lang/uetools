a8cf5574c45e30058a7287c2b8e0b3d7d348b923
Setepenre <pierre.delaunay@outlook.com>
2024-03-27 20:13:55 -0400

    Checkout submodules

--- uetools/core/__init__.py
+++ uetools/core/__init__.py
@@ -1,7 +1,7 @@
 """Top level module for uetools"""
 
 __descr__ = "Unreal Engine Tools"
-__version__ = "2.1.0"
+__version__ = "2.1.1"
 __license__ = "BSD 3-Clause License"
 __author__ = "Pierre Delaunay"
 __author_email__ = "pierre@delaunay.io"
